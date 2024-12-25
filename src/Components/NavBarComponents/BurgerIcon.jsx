37e44ca2ad353772a69a4e4f95d432e89dc58996
Kai Mitchell <kaimitchell.personal@gmail.com>
2024-12-25 19:08:44 +1300

    Figure out how I can implement horizontal scroll on the header without having the drop down overflow inside of the header

--- src/Components/NavBarComponents/BurgerIcon.jsx
+++ src/Components/NavBarComponents/BurgerIcon.jsx
@@ -4,7 +4,7 @@ function BurgerIcon({ isNavDropDown, setIsNavDropDown }) {
     const rotate = isNavDropDown ? 'origin-center rotate-90' : '';
 
     return(
-        <button onClick={() => setIsNavDropDown(!isNavDropDown)} className={`${rotate} hidden text-white`} >
+        <button onClick={() => setIsNavDropDown(!isNavDropDown)} className={`${rotate} sm:hidden text-white`} >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className='size-10'>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
                 </svg>
--- src/Components/NavBarComponents/NavOptions.jsx
+++ src/Components/NavBarComponents/NavOptions.jsx
@@ -16,10 +16,10 @@ function NavOptions({ username, setUser, setFilter, setIsSettings }) {
     ];
 
     return(
-        <nav className='relative w-full flex justify-between sm:px-2.5 shadow-xl'>
+        <nav className='relative w-full flex justify-between sm:px-2.5 shadow-xl overflow-x-auto'>
             <MobileOptions isNavDropDown={isNavDropDown} setIsNavDropDown={setIsNavDropDown} />
             <div className={`${isNavDropDown ? 'block' : 'hidden'} absolute sm:contents w-full sm:w-full sm:right-2.5 top-full flex flex-col gap-5 sm:flex-row sm:justify-between items-center bg-black sm:bg-transparent text-white sm:text-black p-5 sm:p-0`}>
-                {categories.map((category, index) => <HeaderButton key={category} category={category} text={category} canHover={true} isLink={false} setFilter={setFilter} showRight={index > categories.length - 3 ? true : false} />)}
+                {categories.map((category, index) => <HeaderButton key={category} category={category} text={category} canHover={true} isLink={false} setFilter={setFilter} showRight={index > 1 ? true : false} />)}
                 {username === '' && <HeaderButton text="Register" path="/register" isLink={true}  />}
                 {username === '' && <HeaderButton text="Sign in" path="/sign-in" isLink={true}  />}
                 <ProfileDropDownContainer setUser={setUser} username={username} setIsSettings={setIsSettings} />
