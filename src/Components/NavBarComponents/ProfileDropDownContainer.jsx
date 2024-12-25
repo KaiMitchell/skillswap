c57c7e70607ca9217edf71d42947f734f5e49a4e
Kai Mitchell <kaimitchell.personal@gmail.com>
2024-12-25 19:24:58 +1300

    Cant fugure out how to horizontal overflow scroll header.

--- src/Components/NavBarComponents/ProfileDropDownContainer.jsx
+++ src/Components/NavBarComponents/ProfileDropDownContainer.jsx
@@ -5,8 +5,8 @@ function ProfileDropDownContainer({ username, setUser,  setIsSettings }) {
     const [isShown, setIsShown] = useState();
 
     return(
-        <div className={`${username !== '' ? 'block' : 'hidden'} sticky right-0  z-20 flex flex-col items-center justify-center p-5 bg-stone-900 shadow-[-7px_0px_10px_0px_black;] hover:bg-stone-700`} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
-            <p className='text-white m-1 self-center hover:bg-stone-700'>{username?.slice(0, 1).toUpperCase()}</p>
+        <div className={`${username !== '' ? 'block' : 'hidden'} sticky right-0 z-20 flex flex-col items-center justify-center py-2.5 px-5 bg-stone-900 shadow-[-7px_0px_10px_0px_black;] hover:bg-stone-700`} onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+            <p className='text-white self-center hover:bg-stone-700'>{username?.slice(0, 1).toUpperCase()}</p>
             <ProfileDropDown setUser={setUser} username={username} isShown={isShown} setIsShown={setIsShown} setIsSettings={setIsSettings} />
         </div>
     );
--- src/Sections/Header.jsx
+++ src/Sections/Header.jsx
@@ -3,7 +3,7 @@ import NavBar from "../Components/NavBarComponents/NavBar";
 function Header({ username, setUser, setFilter, setIsSettings }) {
 
     return(
-        <header className='h-full pt-10 md:pt-5 border-b'>
+        <header className='h-full pt-10 border-b'>
             <div className='fixed top-0 z-20 w-full bg-stone-900'>
                 <NavBar username={username} setUser={setUser} setFilter={setFilter} setIsSettings={setIsSettings} />
             </div>
