e03551543e937e43628f75119dd24947203495ea
Kai Mitchell <kaimitchell.personal@gmail.com>
2025-01-13 00:30:22 +1300

    remove unexesarry card skills component

--- src/Components/Profile/CardDetails.jsx
+++ src/Components/Profile/CardDetails.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from 'react';
 import { Details } from './CardData';
-import CardSkillsContainer from './CardSkillsContainer';
+import CardSkills from './CardSkills';
+import Button from '../../commonComponents/Button';
 
 function CardDetails({ username, description }) {
     const [isRenderAllSkills, setIsRenderAllSkills] = useState(false);
     const [toLearnProfileData, setToLearnProfileData] = useState();
     const [toTeachProfileData, setToTeachProfileData] = useState();
+
+    const showSkillsToggleBg = {
+        hover: 'hover:from-blue-600 hover:via-blue-800 hover:to-blue-800',
+        initial: 'from-blue-400 via-blue-500 to-blue-600'
+    };
 
     useEffect(() => {
         isRenderAllSkills && fetchProfileSkills();
@@ -35,12 +41,23 @@ function CardDetails({ username, description }) {
                 description={description}
                 isRenderAllSkills={isRenderAllSkills}       
             />
-            <CardSkillsContainer 
-                toLearnProfileData={toLearnProfileData} 
-                toTeachProfileData={toTeachProfileData} 
-                isRenderAllSkills={isRenderAllSkills}
-                setIsRenderAllSkills={setIsRenderAllSkills}
-            />
+            <div className='h-full w-full flex flex-col justify-between'>
+                <CardSkills 
+                    profileData={toLearnProfileData} 
+                    isRenderAllSkills={isRenderAllSkills} 
+                    type={'To learn:'} 
+                />
+                <CardSkills 
+                    profileData={toTeachProfileData} 
+                    isRenderAllSkills={isRenderAllSkills} 
+                    type={'To teach:'} 
+                />
+                <Button 
+                    styles={`${isRenderAllSkills ? 'relative rounded-t-lg': 'absolute top-0 rounded-b-lg'} bg-gradient-to-r ${showSkillsToggleBg.hover} ${showSkillsToggleBg.initial} w-1/2 self-center text-sm text-white cursor-pointer`}
+                    handleOnClick={() => setIsRenderAllSkills(!isRenderAllSkills)}
+                    text={isRenderAllSkills ? 'Hide all skills' : 'Show all skills'}
+                />
+            </div>
         </div>
     );
 };
