48ac31249e22acdd195d0aa3742ebf732221dab1
Kai Mitchell <kaimitchell.personal@gmail.com>
2025-01-13 00:34:57 +1300

    remove unecessary file

--- src/Components/Profile/CardDetails.jsx
+++ src/Components/Profile/CardDetails.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Details } from './CardData';
 import CardSkills from './CardSkills';
 import Button from '../../commonComponents/Button';
 
@@ -37,10 +36,17 @@ function CardDetails({ username, description }) {
 
     return(
         <div className='relative h-full w-full flex flex-col justify-between'>
-            <Details 
-                description={description}
-                isRenderAllSkills={isRenderAllSkills}       
-            />
+            <div className={`${isRenderAllSkills ? 'hidden' : 'block'} flex flex-col gap-2.5 mt-10 h-full w-full px-5`}>
+                <div className='flex gap-2.5 items-center'>
+                    <h3>Preferrence:</h3>
+                    <p className='text-xs'>In person</p>
+                </div>
+                <div className='h-16'>
+                    <div className='relative h-full overflow-y-auto no-scrollbar'>
+                        <p className=''>{description}</p>
+                    </div>
+                </div>
+            </div>
             <div className='h-full w-full flex flex-col justify-between'>
                 <CardSkills 
                     profileData={toLearnProfileData} 
