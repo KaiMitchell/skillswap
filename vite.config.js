737af96e64519a06c9d12674b50892709178958a
Kai Mitchell <kaimitchell.personal@gmail.com>
2025-01-25 08:22:00 +1300

    lkk

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     port: parseInt(process.env.PORT) || 4173, // Ensure it uses the `PORT` environment variable
-    host: '0.0.0.0', // Bind to all network interfaces
+    host: '0.0.0.0', // Bind to all network interfaces,
+    strictPort: true, // Ensure the specified port is used
+    allowedHosts: ['skillswap-wxvl.onrender.com'], // Allow Render's host
   },
 })
