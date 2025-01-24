2f2945ab3b25fd9f9b623e35c964527c39ebe976
Kai Mitchell <kaimitchell.personal@gmail.com>
2025-01-25 08:10:23 +1300

    update allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   preview: {
     port: parseInt(process.env.PORT) || 4173, // Ensure it uses the `PORT` environment variable
     host: '0.0.0.0', // Bind to all network interfaces
+    allowedHosts: 'https://skillswap-wxvl.onrender.com'
   },
 })
