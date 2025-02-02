85803e85814b201625a0082eb03f0052101f51b3
Kai Mitchell <kaimitchell.personal@gmail.com>
2025-02-03 12:02:29 +1300

    place bg on the body

--- src/Components/MainPanel/Cards/CardPanel.jsx
+++ src/Components/MainPanel/Cards/CardPanel.jsx
@@ -40,7 +40,7 @@ function CardPanel({
         />
 
     return(
-        <section id='profile-cards' className='flex flex-col gap-2.5 pt-2.5 min-h-screen grow w-full bg-stone-200'>
+        <section id='profile-cards' className='flex flex-col gap-2.5 min-h-screen h-full grow w-full'>
             {isLoading && <Loading feedBack={'Loading'} />}
             <div className='relative grid grid-cols-1 sm:grid-cols-2 gap-2.5 max-w-full'>
                 {isToLearnProfiles ? mappedLearnProfiles : mappedTeachProfiles}   
